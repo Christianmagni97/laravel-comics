085932a64d31cfd289f56e71a52fd6462feec993
Christianmagni97 <christianmagni28@gmail.com>
2024-07-17 22:25:38 +0200

    passi general

---
+++ laravel-comics/vite.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+            'resources/scss/app.scss',
+            'resources/js/app.js',
+        ],
+        refresh: true
+    }),
+    ],
+
+    resolve: {
+        alias: {
+                '~resources' : '/resources/'
+
+        }
+    }
+});
+'~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
